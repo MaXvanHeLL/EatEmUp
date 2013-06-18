292e1055dae28b60f66f4b827cdbb71d18710e96
tecHunt <kampichler@student.tugraz.at>
2013-06-19 01:25:02 +0200

    Testfiles

---
+++ src/com/android/eatemupgame/CalculationTest.java
@@ -0,0 +1,43 @@
+package com.android.eatemupgame;
+
+import junit.framework.TestCase;
+
+public class CalculationTest extends TestCase {
+
+	public void testCalculation() {
+		fail("Not yet implemented");
+	}
+
+	public void testCalculate() {
+		fail("Not yet implemented");
+	}
+
+	public void testCalcTurnDir() {
+		fail("Not yet implemented");
+	}
+
+	public void testMovePlayer() {
+		fail("Not yet implemented");
+	}
+
+	public void testMoveEnemy() {
+		fail("Not yet implemented");
+	}
+
+	public void testJumpScreenRight() {
+		fail("Not yet implemented");
+	}
+
+	public void testJumpScreenLeft() {
+		fail("Not yet implemented");
+	}
+
+	public void testJumpScreenDown() {
+		fail("Not yet implemented");
+	}
+
+	public void testJumpScreenUp() {
+		fail("Not yet implemented");
+	}
+
+}
